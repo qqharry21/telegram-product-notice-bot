d4bc097c7c6b3fca513f531c9aa79c09d1cb12cd
qqharry21 <qqharry21@gmail.com>
2024-05-24 17:09:40 +0800

    fix: remove flack

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 import requests
-import asyncio
 from bs4 import BeautifulSoup
 from telegram import Bot
 import schedule
 import time
-from flask import Flask, request
 import os
-
-app = Flask(__name__)
 
 def check_product_status(url):
     response = requests.get(url)
@@ -17,13 +13,14 @@ def check_product_status(url):
     # 檢查是否存在class為'g-btn btn-cart'的按鈕
     button_element = soup.find('button', class_='g-btn btn-cart')
     if button_element:
-        print('FOUND')
+        print('商品已開放訂購！')
         return True
+    print('商品尚未開放訂購！')
     return False
 
 def send_telegram_message(message, token, chat_id):
     bot = Bot(token=token)
-    asyncio.run(bot.send_message(chat_id=chat_id, text=message))
+    bot.send_message(chat_id=chat_id, text=message)
 
 # 獲取chat_id
 def get_chat_id(token):
@@ -34,33 +31,16 @@ def get_chat_id(token):
     else:
         raise Exception('No messages found. Please send a message to your bot and try again.')
 
-PRODUCT_URL = 'https://www.5music.com.tw/CDList-C.asp?cdno=445425679120'
-TELEGRAM_TOKEN = '7110618214:AAGSwNTTPnC0VaJLgrGApc9Vw2KKfK89ESg'  # 替換為你的Telegram Bot權杖
+PRODUCT_URL = os.environ.get('PRODUCT_URL', 'https://www.5music.com.tw/CDList-C.asp?cdno=445425679120')
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '7110618214:AAGSwNTTPnC0VaJLgrGApc9Vw2KKfK89ESg')
 CHAT_ID = get_chat_id(TELEGRAM_TOKEN)  # 獲取你的Chat ID
 
 def job():
     if check_product_status(PRODUCT_URL):
         send_telegram_message("商品已開放訂購！", TELEGRAM_TOKEN, CHAT_ID)
 
-# Flask route to trigger the job manually
-@app.route('/check', methods=['GET'])
-def check():
-    job()
-    return "Check completed."
+schedule.every(1).minutes.do(job)  # 每10分鐘檢查一次
 
-schedule.every(1).minutes.do(job)  # 每1分鐘檢查一次
-
-def run_scheduler():
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-if __name__ == '__main__':
-    import threading
-    # Start the scheduler in a separate thread
-    scheduler_thread = threading.Thread(target=run_scheduler)
-    scheduler_thread.start()
-
-    # Run the Flask app
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
